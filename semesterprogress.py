ce18ad58e91b64f56ef5a8e612f4c220f28bd12b
fussballlinguist <39825770+fussballlinguist@users.noreply.github.com>
2021-11-11 11:02:01 +0100

    Update semesterprogress.py

--- semesterprogress.py
+++ semesterprogress.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#! /usr/bin/env/python
 
 from datetime import datetime, timedelta
 import random
@@ -9,14 +9,14 @@ import random
 ############################################################################
 
 # --> Adjust the beginning and end of the semester
-begin = '12.04.2021\t07:30:00'
-end = '23.07.2021\t20:00:00'
+begin = '11.10.2021\t07:30:00'
+end = '04.02.2022\t20:00:00'
 
 #############################
 # No changes below this line!
 #############################
 
-interjections = ["\\\\o\/","Mein lieber Herr Gesangsverein!","Scheiß die Wand an!","Alter Schwede!","Heidewitzka!","Holla die Waldfee!","WTF!!","Ach du grüne Neune!","Sapperlot!","Ich glaub, mein Schwein pfeift!","Heiliger Bimbam!","Prost Mahlzeit!","Da brat mir einer n Storch!","Ach du meine Nase!","Hör mir auf!","Mannometer!","Ist es zu glauben?","Oh my fucking goodness!","Lecko mio!","Alter!","Gopferdeggl!","Grundgütiger!","Krass!","Juhuu!","Echt jetzt?","Tschakka!","Derbst, Alter!","Gimme five!","Da legst di nieda!","Eieiei!","Heiliger Strohsack!","Dat jibbet doch gar nich!","Ach du grüne Neune!","Schon gehört?","Unglaublich!","Ach du dickes Ei!","Ach du liebes bisschen!","Ach, du Schreck!","Da schau her!","Ist nicht wahr!","Oh Gottogott!","Schockschwerenot!","Schreck lass nach!","Guck an!","Teufel auch!","Wahnsinn!"]
+interjections = ["\\\\o\/","Mein lieber Herr Gesangsverein!","Scheiß die Wand an!","Alter Schwede!","Heidewitzka!","Holla die Waldfee!","WTF!!","Ach du grüne Neune!","Sapperlot!","Ich glaub, mein Schwein pfeift!","Heiliger Bimbam!","Prost Mahlzeit!","Da brat mir einer n Storch!","Ach du meine Nase!","Hör mir auf!","Mannometer!","Ist es zu glauben?","Oh my fucking goodness!","Lecko mio!","Alter!","Gopferdeggl!","Grundgütiger!","Krass!","Juhuu!","Echt jetzt?","Tschakka!","Derbst, Alter!","Gimme five!","Da legst di nieda!","Eieiei!","Heiliger Strohsack!","Dat jibbet doch gar nich!","Ach du grüne Neune!","Schon gehört?","Unglaublich!","Ach du dickes Ei!","Ach du liebes bisschen!","Ach, du Schreck!","Da schau her!","Ist nicht wahr!","Oh Gottogott!","Schockschwerenot!","Schreck lass nach!","Guck an!","Teufel auch!","Wahnsinn!","Oje, ojemine!","Ich werd verrückt!","Da wird doch der Hund in der Pfanne verrückt!","Allmecht!","Oha!","Ich glaub mich laust der Affe!","Ich glaub, mich knutscht ein Elch!","Zum Donnerkeil!","Ich glaub, mein Hamster bohnert!","Meine Fresse!","Alter Falter!","Menschenskinder!","Ach du liebes Lieschen!","Ach du liebe Zeit!","Donnerlittchen!","Jesses, Maria und Joseph!","Heiliger Bimbam!","Heidewitzka!","Ich krieg die Motten!","Ich glaub, mich tritt ein Pferd!","Potz Blitz!"]
 
 fmt = '%d.%m.%Y\t%X'
 period = datetime.strptime(end, fmt) - datetime.strptime(begin, fmt)
@@ -29,5 +29,5 @@ for i in range(1,101):
 	diff = 15 - p
 	interjection = random.choice(interjections)
 	dt += timedelta(seconds=percent)
-	print(f'{dt}\t{interjection}\\n{p * "▓"}{diff * "░"}', end='')
+	print(f'{dt.strftime(fmt)}\t{interjection}\\n{p * "▓"}{diff * "░"}', end='')
 	print(f'\\n{i}% des Semesters sind rum.')
